9fbd82e53f3180c2e272bd83fc40c38ce38bd10d
Harry John <johrry@amazon.com>
2018-12-31 10:30:55 +0530

    Update stats

--- scripts/config.js
+++ scripts/config.js
@@ -63,14 +63,36 @@ var config = {
             "title": "Stats",
             "items": [
                 {
-                    "label": "Hitting the gym",
+                    "label": "Hitting the Gym",
                     "desc": "257 days"
                 },
                 {
-                    "label": "Social media boycott",
-                    "desc": "1 days"
+                    "label": "Social Media boycott",
+                    "desc": diffDays(
+                        new Date(), new Date(Date.parse('December 28, 2018'))
+                    ) + " days"
+                },
+                {
+                    "label": "Time spent on planet Earth",
+                    "desc": diffHours(
+                        new Date(), new Date(Date.parse('February 7, 1993'))
+                    ) + " hours"
                 }
             ]
         }
     ]
+}
+
+function diffDays(firstDate, secondDate) {
+    console.log(firstDate, secondDate);
+    var oneDay = 24*60*60*1000;
+
+    return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+}
+
+function diffHours(firstDate, secondDate) {
+    console.log(firstDate, secondDate);
+    var oneHour = 60*60*1000;
+
+    return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneHour)));
 }
